f871f2b4cec1ad400aa79304afea9e37d8bf75c4
RobS <51037713+RobS30@users.noreply.github.com>
2020-07-20 16:21:12 -0500

    code to compute first generation

--- code.js
+++ code.js
@@ -1,15 +1,34 @@
 var rows = 24;
 var cols = 24;
-
 var playing = false;
+
+var grid = new Array(rows);
+var nextGrid = new Array(rows);
+
+function initializeGrids() {
+    for (var i = 0; i < rows; i++) {
+        grid[i] = new Array(cols);
+        nextGrid[i] = new Array(cols);
+    }
+}
+function resetGrids() {
+    for (var i = 0; i < rows; i++) {
+        for (var j = 0; j < cols; j++) {
+            grid[i][j] = 0;
+            nextGrid[i][j] = 0;
+        }
+    }
+}
 
 // initialize
 function initialize() {
     createTable();
+    initializeGrids();
+    resetGrids();
     setupControlButtons();
 }
 
-// lay out the board
+// lay out the grid 
 function createTable() {
     var gridContainer = document.getElementById("gridContainer");
     if (!gridContainer) {
@@ -31,12 +50,19 @@ function createTable() {
     }
     gridContainer.appendChild(table);
 }
+
 function cellClickHandler() {
+    var rowcol = this.id.split("_");
+    var row = rowcol[0];
+    var col = rowcol[1];
+
     var classes = this.getAttribute("class");
     if (classes.indexOf("live") > -1) {
         this.setAttribute("class", "dead");
+        grid[row][col] = 0;
     } else {
         this.setAttribute("class", "live");
+        grid[row][col] = 1;
     }
 }
 
@@ -57,11 +83,11 @@ function clearButtonHandler() {
     startButton.innerHTML = "start";
 }
 
+// start/pause/continue the game
 function startButtonHandler() {
     if (playing) {
         console.log("Pause the game");
         playing = false;
-        // this gets the value you clicked on in a click handler function
         this.innerHTML = "continue";
     } else {
         console.log("Continue the game");
@@ -70,6 +96,73 @@ function startButtonHandler() {
         play();
     }
 }
+
+// run the life game
+function play() {
+    computeNextGen();
+}
+
+function computeNextGen() {
+    for (var i = 0; i < rows; i++) {
+        for (var j = 0; j < cols; j++) {
+            applyRules(i, j);
+        }
+    }
+}
+
+// this functon works on each cell one at a time that we pass in from computeNextGen()
+// - If a cell has fewer than two living adjacent neighbors then the cell dies, by under-population. If a cell dies, don't mark it on the second sheet.
+// - If a cell has two or three live neighbors, it lives on to the next generation, so mark it on the second sheet.
+// - If a live cell has more than three live neighbors, it dies because of overcrowding, so don't mark it on the second sheet.
+// - Finally, if any dead cell has exactly three live neighbors, it becomes a live cell by reproduction, so mark it as live on the second sheet.
+function applyRules(row, col) {
+    var numNeighbors = countNeighbors(row, col);
+    if (grid[row][col] == 1) {
+        if (numNeighbors < 2) {
+            nextGrid[row][col] = 0;
+        } else if (numNeighbors == 2 || numNeighbors == 3) {
+            nextGrid[row][col] = 1;
+        } else if (numNeighbors > 3) {
+            nextGrid[row][col] = 0;
+        }
+    } else if (grid[row][col] == 0) {
+        if (numNeighbors == 3) {
+            nextGrid[row][col] = 1;
+        }
+    }
+}
+
+
+
+function countNeighbors(row, col) {
+    var count = 0;
+    if (row-1 >= 0) {
+        if (grid[row-1][col] == 1) count++;
+    }
+    if (row-1 >= 0 && col-1 >= 0) {
+        if (grid[row-1][col-1] == 1) count++;
+    }
+    if (row-1 >= 0 && col+1 < cols) {
+        if (grid[row-1][col+1] == 1) count++;
+    }
+    if (col-1 >= 0) {
+        if (grid[row][col-1] == 1) count++;
+    }
+    if (col+1 < cols) {
+        if (grid[row][col+1] == 1) count++;
+    }
+    if (row+1 < rows) {
+        if (grid[row+1][col] == 1) count++;
+    }
+    if (row+1 < rows && col-1 >= 0) {
+        if (grid[row+1][col-1] == 1) count++;
+    }
+    if (row+1 < rows && col+1 < cols) {
+        if (grid[row+1][col+1] == 1) count++;
+    }
+    return count;
+}
+
 
 // start everything
 
